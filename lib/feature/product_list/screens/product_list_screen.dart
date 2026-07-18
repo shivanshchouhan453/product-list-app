70b653e9ef692564b15143094c8bedb50d4b2e62
shivanshchouhan453 <cshivansh453@gmail.com>
2026-07-18 17:54:00 +0530

    add : Clean up the code

--- lib/feature/product_list/screens/product_list_screen.dart
+++ lib/feature/product_list/screens/product_list_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:product_list_app/feature/product_list/screens/product_details_screen.dart';
-import 'package:product_list_app/feature/product_list/services/models/product_details_model.dart';
+import 'package:product_list_app/core/widgets/product_card_widget.dart';
 import 'package:product_list_app/feature/product_list/services/providers/product_list_provider.dart';
 
 class ProductListScreen extends ConsumerStatefulWidget {
@@ -22,7 +21,6 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
   @override
   void initState() {
     super.initState();
-    print("loading more..");
     _scrollController.addListener(() {
       // when it will reach it to bottom the fetch again more product and append it
       if (_scrollController.position.pixels >=
@@ -82,7 +80,6 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
         ),
         body: RefreshIndicator(
           onRefresh: () async {
-            print("Refreshing...");
             await notifier.refreshProduct();
           },
           child: Container(
@@ -106,7 +103,6 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
                       autocorrect: true,
                       controller: searchProductController,
                       decoration: InputDecoration(
-                        // label: Icon(Icons.search),
                         hintText: "Search for product",
                         hintStyle: const TextStyle(color: Colors.black54),
                         border: OutlineInputBorder(borderRadius: .circular(30)),
@@ -143,7 +139,6 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
                                       .changeCategory(category);
                                 },
                                 child: Container(
-                                  // width: 100,
                                   padding: EdgeInsets.all(10),
                                   decoration: BoxDecoration(
                                     borderRadius: .circular(20),
@@ -188,14 +183,6 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
                         controller: _scrollController,
                         itemCount: productList.length,
                         itemBuilder: (context, index) {
-                          // if (index == productList.length) {
-                          //   return const Center(
-                          //     child: Padding(
-                          //       padding: EdgeInsets.all(16),
-                          //       child: CircularProgressIndicator(),
-                          //     ),
-                          //   );
-                          // }
                           final product = productList[index];
 
                           return productCard(product, context);
@@ -246,216 +233,4 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
       ),
     );
   }
-}
-
-Widget productCard(Product product, BuildContext context) {
-  return Card(
-    elevation: 0,
-    color: Colors.white,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(24),
-      side: BorderSide(color: Colors.grey.shade200),
-    ),
-    child: InkWell(
-      borderRadius: BorderRadius.circular(24),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ProductDetailsScreen(product: product),
-          ),
-        );
-      },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          /// IMAGE
-          Stack(
-            children: [
-              Hero(
-                tag: product.id,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(24),
-                  ),
-                  child: AspectRatio(
-                    aspectRatio: 2,
-                    child: Image.network(
-                      product.thumbnail,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-
-              Positioned(
-                left: 12,
-                top: 12,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 5,
-                    vertical: 3,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Text(
-                    "-${product.discountPercentage.toStringAsFixed(0)}%",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-              ),
-
-              Positioned(
-                right: 12,
-                top: 12,
-                child: CircleAvatar(
-                  radius: 18,
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.favorite_border, size: 20),
-                ),
-              ),
-            ],
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 16,
-              left: 10,
-              right: 10,
-              bottom: 16,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 13,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                // const SizedBox(height: 5),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        product.brand ?? product.category,
-                        style: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                        vertical: 4,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.amber.shade100,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.star,
-                            size: 12,
-                            color: Colors.orange,
-                          ),
-                          const SizedBox(width: 3),
-                          Text(
-                            product.rating.toStringAsFixed(1),
-                            style: TextStyle(fontSize: 12, fontWeight: .w600),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 8),
-
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 3,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.blue.shade50,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Text(
-                    product.category,
-                    style: TextStyle(
-                      color: Colors.blue.shade700,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 8),
-
-                Row(
-                  children: [
-                    Text(
-                      "\$${product.price}",
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                    const SizedBox(width: 8),
-
-                    Text(
-                      "\$${(product.price / (1 - product.discountPercentage / 100)).toStringAsFixed(2)}",
-                      style: TextStyle(
-                        decoration: TextDecoration.lineThrough,
-                        color: Colors.grey.shade500,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 12),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: product.stock > 10
-                        ? Colors.green.shade100
-                        : Colors.orange.shade100,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Text(
-                    product.stock > 10 ? "In Stock" : "Few Left",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: product.stock > 10
-                          ? Colors.green.shade700
-                          : Colors.orange.shade700,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
 }
